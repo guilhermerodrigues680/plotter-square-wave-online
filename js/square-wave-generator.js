75505b6bb526df7fade4a2b34d8e46fd69b58cd8
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-05-14 13:48:15 -0300

    Implementado Onda Quadrada e Onda Quadrada PWM

--- js/square-wave-generator.js
+++ js/square-wave-generator.js
@@ -35,14 +35,15 @@ function generateSquareWave(f, T) {
 }
 
 
-function plotSquareWave(f, T) {
-    const wave = generateSquareWave(f, T);
+function plotSquareWave(f, T, dutyCicle=null) {
+    const wave = dutyCicle === null ? generateSquareWave(f, T) : generateSquareWavePWM(f, T, dutyCicle);
+
     console.log(wave);
     // Ex: Plotly.newPlot('myDiv', data, layout, {scrollZoom: true});
 
     let data = [];
     const layout = {
-        title: 'Onda Quadrada',
+        title: dutyCicle === null ? 'Onda Quadrada' : 'Onda Quadrada PWM',
         xaxis: { title: wave.titleX },
         yaxis: { title: wave.titleY },
     }
@@ -57,4 +58,39 @@ function plotSquareWave(f, T) {
 
     // margin: { t: 0 },
     Plotly.newPlot( PlotWaveAreaDOM, data, layout);
+    return waveInfo;
+}
+
+
+function generateSquareWavePWM(f, T, dutyCicle){
+    if (T < 1){
+        throw ('O perido deve ser inteiro > 0');
+    }
+
+    const titleX = 'Tempo(s)';
+    const titleY = 'Tensão(V)';
+    const p = 1/f;
+    // x = [0, p*(dutyCicle/100), p*(dutyCicle/100), p, p]
+    // y = [5, 5, 0, 0, 5]
+    let x =[], y =[];
+
+    for (let i = 0; i < T; i++) {
+        console.debug(i);
+        x = x.concat([p*i, (p*i)+p*(dutyCicle/100), (p*i)+p*(dutyCicle/100), p*(i+1), p*(i+1)]);
+        y = y.concat([5, 5, 0, 0, 5]);   
+    }
+
+    waveInfo = `Frequencia: ${f}Hz :: Periodo: ${p}s :: DutyCicle ${dutyCicle}% :: Nº periodos plotados: ${T}`;
+
+    res = {
+        'x': x,
+        'y': y,
+        'titleX': titleX,
+        'titleY': titleY,
+        'waveInfo': waveInfo,
+    };
+
+    console.debug(res);
+    console.log(waveInfo);
+    return res;
 }
--- js/square-wave-with-duty-cicle.js
+++ js/square-wave-with-duty-cicle.js
@@ -1,24 +1,27 @@
 const BtnPlotWaveDOM = document.querySelector('#btn-plot-wave');
 const WaveFrequencyDOM = document.querySelector('#input-wave-frequency');
 const WavePeriodsDOM = document.querySelector('#input-wave-periods');
+const WaveDutyCicleDOM = document.querySelector('#input-wave-dutycicle');
 const PwaveInfoDOM = document.querySelector('#p-wave-info');
 
 BtnPlotWaveDOM.addEventListener('click', callPlotSquareWave);
 WaveFrequencyDOM.addEventListener('change', callPlotSquareWave);
 WavePeriodsDOM.addEventListener('change', callPlotSquareWave);
+WaveDutyCicleDOM.addEventListener('change', callPlotSquareWave);
 
 function callPlotSquareWave(e) {
     const f = WaveFrequencyDOM.value;
     const T = WavePeriodsDOM.value;
+    const dutyCicle = WaveDutyCicleDOM.value;
 
-    const waveInfo = plotSquareWave(f, T);
+    const waveInfo = plotSquareWave(f, T, dutyCicle);
     PwaveInfoDOM.innerText = waveInfo;
 }
 
 
 function defaultPlot() {
-    // Plot default 1Hz , 2 Periods
-    const waveInfo = plotSquareWave(1, 2);
+    // Plot default 1Hz , 2 Periods, 20% Duty Cicle
+    const waveInfo = plotSquareWave(1, 2, 20);
     PwaveInfoDOM.innerText = waveInfo;
 }
 
